a6dd86cfb94ea7828f85d44c9741dff68bcf9f17
alejandr0gre1ff <alegreiff181@gmail.com>
2022-05-21 21:50:13 -0300

    Previo a correcciones

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -7,23 +7,7 @@ const LinkList = () => {
                 <Link to={'/'}>Inicio</Link>
             </li>
             <li>
-                <Link to={'/category/sneakers'}>
-                    Sneakers
-                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                    </svg>
-                </Link>
-                <ul className="p-2 bg-base-100">
-                    <li>
-                        <a>Nike</a>
-                    </li>
-                    <li>
-                        <a>Addidas</a>
-                    </li>
-                    <li>
-                        <a>Fila</a>
-                    </li>
-                </ul>
+                <Link to={'/category/sneakers'}>Sneakers</Link>
             </li>
             <li>
                 <Link to={'/category/buzos'}>Buzos</Link>
